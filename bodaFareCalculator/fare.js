7763212228e50150e0be21dc9555e08bd1d2083c
saidabdulhakim48@gmail.com <saidabdulhakim48@gmail.com>
2025-06-08 22:01:21 +0300

    Completed Labs

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,1 +1,15 @@
+function calculateBodaFare(distanceInKm) {
+    const baseFare = 50;
+    const chargePerKm = 15;
+    const tripFare = distanceInKm * chargePerKm;
+    const totalFare = baseFare + tripFare;
 
+    console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
+    console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
+    console.log(`Mpaka Uko: KES ${tripFare}`);
+    console.log(`Total: KES ${totalFare}`);
+    console.log("\nPanda Pikipiki!");
+}
+
+const input = prompt("Unafika wapi Mkubwa? Kilometer ngapi?:");
+calculateBodaFare(Number(input));
